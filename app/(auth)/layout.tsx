b0a7088b2fa9d4dbf1e8940aa04471a8c725face
hs-fkisuule <farouk.kisuule@homestars.com>
2024-05-06 12:20:36 -0400

    Authentication with clerk-auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LayOut = ({ children }: { children: React.ReactNode}) => {
   return (
-    <main>
+    <main className='auth'>
       {children}
     </main>
   )
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
 import React from 'react'
 
 const TransformationsPage = () => {
   return (
     <div>
-      HOME
+      <p>HOME</p>
+      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
