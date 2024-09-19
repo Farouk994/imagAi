b8fd2ea43d83b45130c9021126d42bb8151fc3b9
Farouk994 <faroukkisuule55@gmail.com>
2024-09-18 22:46:47 -0400

    Remove console.log

--- app/components/shared/TransformationForm.tsx
+++ app/components/shared/TransformationForm.tsx
@@ -127,7 +127,6 @@ const TransformationForm = ({
           if (newImage) {
             form.reset();
             setImage(data);
-            console.log("setData",setImage(data));
             router.push(`/transformations/${newImage._id}`);
           }
         } catch (err) {
