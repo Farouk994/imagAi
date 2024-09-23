d14c576673747005e0f9924577f7c8525d242d2d
Farouk994 <faroukkisuule55@gmail.com>
2024-09-22 21:04:56 -0400

    fix lint

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,24 +4,24 @@ import { Document, model, models, Schema } from "mongoose";
 import { Types } from "mongoose";
 
 export interface IImage extends Document {
-    title: string;
-    transformationType: string;
-    publicId: string;
-    secureURL: string;
-    width?: number;
-    height?: number;
-    config?: object;
-    transformationURL?: string;
-    aspectRatio?: string;
-    color?: string;
-    prompt?: string;
-    author: {
-        _id: string,
-        firstName: string,
-        lastName: string,
-    };
-    createdAt?: Date;
-    updatedAt?: Date;
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationURL?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
