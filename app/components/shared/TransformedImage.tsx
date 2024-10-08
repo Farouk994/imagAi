438b9c25332b02e3cc31bbc567a8108efc503e84
Farouk994 <faroukkisuule55@gmail.com>
2024-10-07 21:21:12 -0400

    fix transformation form

--- app/components/shared/TransformedImage.tsx
+++ app/components/shared/TransformedImage.tsx
@@ -4,6 +4,7 @@ import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import React from "react";
 
+
 const TransformedImage = ({
   image,
   type,
