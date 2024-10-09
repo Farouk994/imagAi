9e6ce4ecb06c37d396aa12f77f03130e2c3fe4e5
Farouk994 <faroukkisuule55@gmail.com>
2024-10-08 20:12:16 -0400

    fix collection search component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,16 +6,17 @@ import React from "react";
 import { Collection } from "../components/shared/Collection";
 import { getAllImages } from "@/lib/actions/image.actions";
 
-const TransformationsPage = async ({ searchParams }: SearchParamProps) => {
+const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  const images = await getAllImages({page, searchQuery})
+  const images = await getAllImages({ page, searchQuery})
+
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash your creative vision with Imaginify
+          Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -32,15 +33,57 @@ const TransformationsPage = async ({ searchParams }: SearchParamProps) => {
           ))}
         </ul>
       </section>
-      <section className="sm:m-12">
-        <Collection hasSearch={true}
+
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
           page={page}
         />
       </section>
     </>
-  );
-};
+  )
+}
 
-export default TransformationsPage;
+export default Home
+
+// const TransformationsPage = async ({ searchParams }: SearchParamProps) => {
+//   const page = Number(searchParams?.page) || 1;
+//   const searchQuery = (searchParams?.query as string) || "";
+
+//   const images = await getAllImages({ page, searchQuery });
+//   return (
+//     <>
+//       <section className="home">
+//         <h1 className="home-heading">
+//           Unleash your creative vision with Imaginify
+//         </h1>
+//         <ul className="flex-center w-full gap-20">
+//           {navLinks.slice(1, 5).map((link) => (
+//             <Link
+//               key={link.route}
+//               href={link.route}
+//               className="flex-center flex-col gap-2"
+//             >
+//               <li className="flex-center w-fit rounded-full bg-white p-4">
+//                 <Image src={link.icon} alt="image" width={24} height={24} />
+//               </li>
+//               <p className="p-14-medium text-center text-white">{link.label}</p>
+//             </Link>
+//           ))}
+//         </ul>
+//       </section>
+//       <section className="sm:m-12">
+//         <Collection
+//           hasSearch={true}
+//           images={images?.data}
+//           totalPages={images?.totalPage}
+//           page={page}
+//         />
+//       </section>
+//     </>
+//   );
+// };
+
+// export default TransformationsPage;
