dae61edd8372fe4cf2bc25206d03f1a28fb02bf3
Farouk994 <faroukkisuule55@gmail.com>
2024-10-30 13:38:08 -0400

    fix html error in layout while deploying

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
+        <title>Imaginify</title> 
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
         </body>
