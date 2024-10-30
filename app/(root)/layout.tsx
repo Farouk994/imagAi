f3e7d620304781fe68e47fa5ea7cfcb0680ccd0a
Farouk994 <faroukkisuule55@gmail.com>
2024-10-29 20:37:37 -0400

    updating fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import SideBar from '../components/shared/SideBar';
-import MobileNav from '../components/shared/MobileNav';
-import { Toaster } from '@/components/ui/toaster';
+import React from "react";
+import SideBar from "../components/shared/SideBar";
+import MobileNav from "../components/shared/MobileNav";
+import { Toaster } from "@/components/ui/toaster";
 
-const LayOut = ({ children }: { children: React.ReactNode}) => {
+const LayOut = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
-      <SideBar/>
-      <MobileNav/>
-      <div className='root-container'>
-        <div className='wrapper'>{children}</div>
+    <main className="root">
+      <SideBar />
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
       </div>
-      <Toaster/>
+      <Toaster />
     </main>
-  )
-}
+  );
+};
 
 export default LayOut;
